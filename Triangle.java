413a1d15dcb1d4f4660dc68b60f9f5945cecbc9a
SuprajaTimmareddy <timmareddysupraja@gmail.com>
2023-12-13 18:49:35 +0530

    Adding Pattern

---
+++ Triangle.java
@@ -0,0 +1,12 @@
+package JavaPatterns;
+
+public class Triangle {
+    public static void main(String[] args) {
+        int m=4;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }System.out.println();
+        }
+    }
+}
